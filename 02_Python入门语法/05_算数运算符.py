7362e26cb2bee7b39b619256b78a05b6ac8d3229
daijiahao81 <daijiahao81@gmail.com>
2026-07-10 21:32:00 +0800

    算数运算符and字符串扩展

---
+++ 02_Python入门语法/05_算数运算符.py
@@ -0,0 +1,27 @@
+# 算数（数学）运算符
+print("1+1=",1+1)
+print("2-1=",2-1)
+print("2*3=",2*3)
+print("4/2=",4/2)  # 除法，结果是浮点数
+print("11//2=",11//2)  # 整除，结果是整数
+print("11%2=",11%2)  # 取余，结果是余数
+print("9%2=",9%2)  # 取余，结果是余数
+print("2**3=",2**3)  # 幂运算，2的3次方
+# 赋值运算符
+num = 1+2*3
+num +=1
+print("num += 1:",num)
+num -= 1
+print("num -= 1:",num)
+num *= 4
+print("num *= 4:",num)
+num /= 2
+print("num /= 2:",num)
+num = 3
+num %= 2
+print("num %= 2:",num)
+num ** 2
+print("num ** 2:",num)
+num = 9
+num //= 2
+print("num //= 2:",num)
