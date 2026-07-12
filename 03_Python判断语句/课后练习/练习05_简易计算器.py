a370340ae78bfc5348ab9389f27332ae1a22233c
daijiahao81 <daijiahao81@gmail.com>
2026-07-12 17:56:01 +0800

    修改第三章Python判断语句-课后练习-简易计算器-修复bug

--- 03_Python判断语句/课后练习/练习05_简易计算器.py
+++ 03_Python判断语句/课后练习/练习05_简易计算器.py
@@ -1,7 +1,24 @@
+# # 定义变量
+# first_number = int(input("请输入第一个数字:"))
+# operator = input("请输入运算符(+、-、*、/):")
+# second_number = int(input("请输入第二个数字:"))
+
+
+# # 用if语句进行判断计算
+# if operator == "+":
+#     print(first_number + second_number)
+# elif operator == "-":
+#     print(first_number - second_number)
+# elif operator == "*":
+#     print(first_number * second_number)
+# else:
+#     print(first_number / second_number)    
+
+# 修改后代码
 # 定义变量
-first_number = int(input("请输入第一个数字:"))
+first_number = float(input("请输入第一个数字:"))
 operator = input("请输入运算符(+、-、*、/):")
-second_number = int(input("请输入第二个数字:"))
+second_number = float(input("请输入第二个数字:"))
 
 
 # 用if语句进行判断计算
@@ -11,5 +28,10 @@ elif operator == "-":
     print(first_number - second_number)
 elif operator == "*":
     print(first_number * second_number)
+elif operator == "/":
+    if second_number == 0:
+        print("除数不能为零")
+    else:
+        print(first_number / second_number)
 else:
-    print(first_number / second_number)    
+    print("暂不支持该运算")
